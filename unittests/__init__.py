bb4e361ee97c9c1e097c371f68e5b9ba2bfefa81
Michael Olberg <michael.olberg@chalmers.se>
2022-05-30 10:27:54 +0200

    make sure import of casatools works in unittest

---
+++ unittests/__init__.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append("/home/data/casa-6.4.0-16/lib/py/lib/python3.8/site-packages")
+sys.path.append(".")
---
+++ unittests/test_uvmultifit.py
@@ -0,0 +1,4 @@
+from NordicARC import uvmultifit as uvm
+
+def test_uvmultifit_init():
+    assert 1 == 1
