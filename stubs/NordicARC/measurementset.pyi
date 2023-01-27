c4687d334549cf6ffa34cc3ba81e34c7a3b14871
Michael Olberg <michael.olberg@chalmers.se>
2023-01-27 14:28:08 +0100

    add more type hints to stub files

--- stubs/NordicARC/measurementset.pyi
+++ stubs/NordicARC/measurementset.pyi
@@ -1,5 +1,7 @@
 from typing import List
 
+from .modeler import Modeler
+
 class MeasurementSet():
 
     def __init__(self, vis: str, spw: str = '0', field: int = 0, scans: List = [],
@@ -8,4 +10,25 @@ class MeasurementSet():
                  MJDrange: List[float] = [-1.0, -1.0], ldfac: float = 1.22,
                  phase_center: str = '', pbeam: bool = False, wgt_power: float = 1.0,
                  dish_diameter: float = 0.0) -> None:
+
+        self.vis = vis
+        self.pbeam = pbeam
+        self.dish_diameter = dish_diameter
+
+        self.spwlist = []                        # type: List
+        self.antnames = []                       # type: List
+        self.userDiameter = []                   # type: List
+        self.tArr = []                           # type: List
+        self.averfreqs = []                      # type: List
+        self.refpos = [0.0, 0.0]                 # type: List[float]
+        self.Nspw = 0                            # type: int
+        ...
+
+    def check_measurementset(self) -> bool:
+        ...
+
+    def read_data(self, takeModel: bool = False) -> bool:
+        ...
+
+    def write_model(self, column: str, model: Modeler) -> bool:
         ...
--- stubs/NordicARC/modeler.pyi
+++ stubs/NordicARC/modeler.pyi
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+from .measurementset import MeasurementSet
+
 class Modeler():
     def __init__(self, model: List[str] = ['delta'], var: List[str] = ['p[0], p[1], p[2]'],
                  p_ini: List[float] = [0.0, 0.0, 1.0], bounds: List = None,
@@ -10,4 +12,26 @@ class Modeler():
                  LMtune: List[float] = [1.e-3, 10., 1.e-5, 200, 1.e-3],
                  SMPtune: List[float] = [1.e-4, 1.e-1, 200],
                  NCPU: int = 4, proper_motion: float = 0.0) -> None:
+        self.OneFitPerChannel = OneFitPerChannel
+        self.model = model
+        self.var = var
+        self.fixed = fixed
+        self.takeModel: bool = 'model_column' in self.fixed
+        self.fixed = fixed
+        self.fixedvar = fixedvar
+        self.scalefix = scalefix
+        self.p_ini = p_ini
+        self.bounds = bounds
+        self.bounds = bounds
+        self.p_ini = p_ini
+
+        ...
+
+    def init_data(self, nspw: int, ms: MeasurementSet) -> bool:
+        ...
+
+    def init_model(self, nspw: int, tArr: List, averfreqs: List, refposnspw: List[float]) -> bool:
+        ...
+
+    def fit(self, ms: MeasurementSet, write_model: int, cov_return: bool, redo_fixed: bool = True, reset_flags: bool = False):
         ...
